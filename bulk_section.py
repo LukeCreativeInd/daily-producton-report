bd3aee08264122ff6ee016dde76016880f7a616a
LukeCreativeInd <71154774+LukeCreativeInd@users.noreply.github.com>
2025-11-30 15:41:13 +1100

    Update bulk_section.py

--- bulk_section.py
+++ bulk_section.py
@@ -9,7 +9,7 @@ bulk_sections = [
     {"title": "Penne Order", "batch_ingredient": "Penne", "batch_size": 135,
      "ingredients": {"Penne": 59, "Oil": 0.7},
      "meals": ["Chicken Pesto Pasta", "Chicken and Broccoli Pasta"]},
-    {"title": "Rice Order", "batch_ingredient": "Rice", "batch_size": 500,
+    {"title": "Rice Order", "batch_ingredient": "Rice", "batch_size": 180,
      "ingredients": {"Rice": 53, "Water": 95, "Salt": 1, "Oil": 1.5},
      "meals": [
          "Beef Chow Mein",
